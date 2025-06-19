b4db24323b8c1914b55c0649557d78685bea4c74
tsents <tsents.sagie@gmail.com>
2025-06-19 15:06:03 +0300

    Add test to mul by 0.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ int main() {
     Complex z1 = Complex(1, 2);
     Complex z2 = Complex(3, 4);
 
-    std::cout << (z1 + z2) << std::endl; // Works
-    std::cout << (z1 - z2) << std::endl; // Works
-    std::cout << (z1 * z2) << std::endl; // Works
+    std::cout << (z1 + z2) << std::endl;                   // Works
+    std::cout << (z1 - z2) << std::endl;                   // Works
+    std::cout << (z1 * z2) << std::endl;                   // Works
+    std::cout << (Complex(1, 2) * Complex()) << std::endl; // Should be 0, 0
     return 0;
 }
